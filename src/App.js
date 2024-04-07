be8838b914c7ff0c80764ef4b62f20c9e4608077
bobbyhadz <opmftw@gmail.com>
2024-04-07 09:04:11 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h2>
+        <span style={{textDecoration: 'line-through'}}>
+          bobby
+        </span>{' '}
+        hadz
+      </h2>
+    </div>
+  );
+};
+
+export default App;
